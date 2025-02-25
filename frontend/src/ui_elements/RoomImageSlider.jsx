5371e3fab51bb5fc3a66b088e5e62be625298353
danyal <danyalshah9009@gmail.com>
2025-02-25 15:43:27 +0500

    Room image slider fixed

--- frontend/src/ui_elements/RoomImageSlider.jsx
+++ frontend/src/ui_elements/RoomImageSlider.jsx
@@ -9,41 +9,55 @@ const RoomImageSlider = ({ roomType }) => {
   const roomImages = {
     masterbed: [
       "/assets/rooms/masterbed111.jpg",
-       "/assets/rooms/masterbed2.jpg",
-       "/assets/rooms/masterbed3.jpg",
-       "/assets/rooms/masterbed4.jpg",
-       "/assets/rooms/masterbed.jpg",
-
-
+      "/assets/rooms/masterbed2.jpg",
+      "/assets/rooms/masterbed3.jpg",
+      "/assets/rooms/masterbed4.jpg",
+      "/assets/rooms/masterbed.jpg",
     ],
     twinbed: [
-        "/assets/rooms/twinbed4.webp",
-        "/assets/rooms/twinbed1.jpg",
-        "/assets/rooms/twinbed2.jpg",
-        "/assets/rooms/twinbed3.webp"],
-
+      "/assets/rooms/twinbed4.webp",
+      "/assets/rooms/twinbed1.jpg",
+      "/assets/rooms/twinbed2.jpg",
+      "/assets/rooms/twinbed3.webp"
+    ],
     triplebed: [
-        "/assets/rooms/triplebed.jpg",
-        "/assets/rooms/triplebed1.jpg",
-        "/assets/rooms/triplebed2.jpg",
-        "/assets/rooms/triplebed3.jpg"],
+      "/assets/rooms/triplebed.jpg",
+      "/assets/rooms/triplebed1.jpg",
+      "/assets/rooms/triplebed2.jpg",
+      "/assets/rooms/triplebed3.jpg"
+    ],
   };
 
   const images = roomImages[roomType] || [];
 
   return (
     <div className="max-w-6xl mx-auto my-10">
+      <style>
+        {`
+          .swiper-button-next,
+          .swiper-button-prev {
+            color: black !important;
+          }
+          
+          .swiper-pagination-bullet {
+            background: gray !important;
+          }
+          
+          .swiper-pagination-bullet-active {
+            background: white !important;
+          }
+        `}
+      </style>
       <Swiper
-  modules={[Navigation, Pagination, Autoplay]}
-  spaceBetween={10}
-  slidesPerView={1}
-  loop={true}
-  autoplay={{ delay: 3000 }}
-  navigation
-  pagination={{ clickable: true }}
-  className="rounded-lg shadow-lg"
->
-
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={10}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 3000 }}
+        navigation
+        pagination={{ clickable: true }}
+        className="rounded-lg shadow-lg"
+      >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
             <img
