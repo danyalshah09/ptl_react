0cbb4cd51eb6463e2122a2db63bf3781d2c4a51b
danyal shah <119670767+danyalshah09@users.noreply.github.com>
2025-03-09 07:15:42 +0500

    Update bookings.js

--- ptl_backend/routes/bookings.js
+++ ptl_backend/routes/bookings.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { createBooking, getAllBookings } = require('../controllers/bookingController');
+const { createBooking } = require('../controllers/bookingController');
 
-// Make sure both route handlers are properly defined
 router.post('/', createBooking);
-// router.get('/', getAllBookings);
 
 module.exports = router;
