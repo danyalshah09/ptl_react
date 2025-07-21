b3cec71b9a61a2bb6f26857bd277329c5105a408
Danyal Shah <danyalshah9009@gmail.com>
2025-07-21 12:42:04 +0500

    desktop

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";  // Import Tailwind CSS
 import autoprefixer from "autoprefixer";  // Import Autoprefixer
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      /* pass your config */
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 80,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 80,
+      },
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],  // Use Tailwind CSS and Autoprefixer
